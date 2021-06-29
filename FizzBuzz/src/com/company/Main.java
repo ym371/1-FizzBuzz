44b63b82f6ad513c2319080cad6405885bfac909
Yuxin Ma <Yuxin.Ma@softwire.com>
2021-06-29 13:26:09 +0100

    part 2: dealt with multiple of 7

--- FizzBuzz/src/com/company/Main.java
+++ FizzBuzz/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    part1();
+	    part2();
     }
     public static void part1(){
 		for(int i = 1 ; i<=100; i++){
@@ -17,6 +17,19 @@ public class Main {
 				System.out.println("Fizz");
 			}
 			else System.out.println(i);
+		}
+	}
+
+	public static void part2(){
+		for(int i = 1 ; i<=200; i++){
+			String output = "";
+			if (i % 3 ==0) output+="Buzz";
+			if (i % 5 == 0) output+= "Fizz";
+			if (i % 7 == 0) output+= "Bang";
+			if (output =="")
+				System.out.println(i);
+			else
+				System.out.println(output);
 		}
 	}
 }
